c142e1948d57ba79c99e1d987377adcf6c553496
Enrique Capellan <enriqcapellan@gmail.com>
2024-08-15 09:40:30 -0400

    set correct voice for scan

--- src/hooks/useSpeak.ts
+++ src/hooks/useSpeak.ts
@@ -1,6 +1,5 @@
 export function useSpeak() {
   const message = new SpeechSynthesisUtterance();
-  message.voice = speechSynthesis.getVoices()[96];
 
   const speak = (text: string) => {
     message.text = text;
